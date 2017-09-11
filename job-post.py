0e4251580b9db50ff14a4588c8f696c2c6219016
au <alexeyu@aspentechlabs.com>
2017-09-11 16:56:15 +0300

    fixes

--- job-post.py
+++ job-post.py
@@ -1,5 +1,6 @@
 import random
 import requests
+from lxml import html
 
 words = requests.get("http://svnweb.freebsd.org/csrg/share/dict/words?view=co&content-type=text/plain").content.splitlines()
 titles = requests.get("https://raw.githubusercontent.com/a1u/job-generator/master/data-title").content.splitlines()
@@ -8,7 +9,10 @@ states = requests.get("https://raw.githubusercontent.com/a1u/job-generator/maste
 cities = requests.get("https://raw.githubusercontent.com/a1u/job-generator/master/data-city").content.splitlines()
 
 #url = "https://www.careersitecloud.com/api/v1/import/55e986c70cf2bf965dabbdec/autest"
-url = "https://www.dev.careersitecloud.com/api/v1/import/55e986c70cf2bf965dabbdec/autest"
+urlpost = "https://www.dev.careersitecloud.com/api/v1/import/55e986c70cf2bf965dabbdec/autest"
+urldelete = "https://www.dev.careersitecloud.com/api/v1/import/job/{id}"
+urllist = "https://www.dev.careersitecloud.com/api/v1/import/job"
+
 headers = {
    'content-type': "application/xml",
     }
@@ -44,31 +48,58 @@ def json2xml(json_obj, line_padding=""):
 
     return "%s%s" % (line_padding, json_obj)
 
-def lambda_handler(event, context):
-    job = { "address" : rws(3, words),
-            "applyEmail" : "no-reply@aspentechlabs.com",
-            "applyRedirect" : "https://www.aspentechlabs.com",
-            "category" : rws(2, words),
-            "city" : rws(1, cities),
-            "country" : rws(1, countries),
-            "description" : rws(200, words),
-            "education" : rws(10, words),
-            "experience" : rws(10, words),
-            "ref" : rws(1, words),
-            "salary" : rws(2, words),
-            "source" : rws(1, words),
-            "state" : rws(2, states),
-            "title" : rws(1, titles),
-            "jobtype" : rws(1, words),
-            "postalcode" : rws(1, words)
- }
-    payload = { "source": { "job": job } }
-    payload = json2xml(payload)
-    response = requests.post(url, headers=headers, auth=('59b2bf0f6bc6bf600654dae3', 'ada408e0-2fe7-4c4c-b34c-76b1702abb03'), data=payload, verify=False)
+def delete(id):
+    response = requests.delete(urldelete.replace("{id}", id), headers=headers, auth=('59b2bf0f6bc6bf600654dae3', 'ada408e0-2fe7-4c4c-b34c-76b1702abb03'), verify=False)
     try:
-        output = response.json()
+        return response.text
     except ValueError:
+        return "Failed"
+
+def lst():
+    response = requests.get(urllist, headers=headers, auth=('59b2bf0f6bc6bf600654dae3', 'ada408e0-2fe7-4c4c-b34c-76b1702abb03'), verify=False)
+    try:
         output = response.text
-    return output
+        tree = html.fromstring(output)
+        return tree.xpath('//item/@id')
+    except ValueError:
+        return []
+
+def post():
+    job = {"address": rws(3, words),
+           "applyEmail": "no-reply@aspentechlabs.com",
+           "applyRedirect": "https://www.aspentechlabs.com",
+           "category": rws(2, words),
+           "city": rws(1, cities),
+           "country": rws(1, countries),
+           "description": rws(200, words),
+           "education": rws(10, words),
+           "experience": rws(10, words),
+           "ref": rws(1, words),
+           "salary": rws(2, words),
+           "source": rws(1, words),
+           "state": rws(2, states),
+           "title": rws(1, titles),
+           "jobtype": rws(1, words),
+           "postalcode": rws(1, words)
+           }
+    payload = {"source": {"job": job}}
+    payload = json2xml(payload)
+    response = requests.post(urlpost, headers=headers,
+                             auth=('59b2bf0f6bc6bf600654dae3', 'ada408e0-2fe7-4c4c-b34c-76b1702abb03'), data=payload,
+                             verify=False)
+    try:
+        return response.text
+    except ValueError:
+        return "Error"
+
+def lambda_handler(event, context):
+    all = lst()
+    target = random.randint(20, 40)
+    if (len(all) > target):
+        for i in range(0, len(all) - target):
+            print delete(all[i])
+    elif (len(all) < target):
+        for i in range(0, target - len(all)):
+            print post()
 
 print lambda_handler(None, None)
